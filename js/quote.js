4af0570637ab1c3758cd46f24ba13cca7d0f256f
Milad Mikeal <miladmikeal@gmail.com>
2019-03-23 18:36:09 -0700

    Add tweet functionality

--- js/quote.js
+++ js/quote.js
@@ -1,14 +1,18 @@
 let apiURL = "https://got-quotes.herokuapp.com/quotes";
 let gotQuote = document.querySelector(".got-quote");
 let gotCharacter = document.querySelector(".got-character");
-
+let tweetLink = document.querySelector(".tweet-link");
 let quoteButton = document.querySelector("#newQuote");
+
 
 function getData() {
     axios.get(apiURL)
         .then(response => {
             gotQuote.innerHTML = '"' + response.data.quote + '"';
             gotCharacter.innerHTML = '-' + response.data.character;
+            let tweet = "https://twitter.com/intent/tweet?";
+            tweet += "text=\"" + encodeURIComponent(response.data.quote).replace(/'/g, "%27") + "\"+—" + encodeURIComponent(response.data.character);
+            tweetLink.href = tweet;
         })
         .catch(error => {
             gotQuote.innerHTML = "(An error has occured)";
