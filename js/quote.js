7b3f5668b6fc5eaedb97fda4b923fa7c07f746a9
Milad Mikeal <miladmikeal@gmail.com>
2019-03-25 18:47:57 -0700

    Adjust img file paths

--- js/quote.js
+++ js/quote.js
@@ -15,61 +15,61 @@ function getData() {
             tweetLink.href = tweet;
             switch (response.data.character) {
                 case "Olenna Tyrell":
-                    image.src = "../img/sigils/tyrell.jpg";
+                    image.src = "/img/sigils/tyrell.jpg";
                     break;
                 case "Jon Snow":
-                    image.src = "../img/sigils/stark.jpg";
+                    image.src = "/img/sigils/stark.jpg";
                     break;
                 case "Bran":
-                    image.src = "../img/sigils/stark.jpg";
+                    image.src = "/img/sigils/stark.jpg";
                     break;
                 case "Sansa":
-                    image.src = "../img/sigils/stark.jpg";
+                    image.src = "/img/sigils/stark.jpg";
                     break;
                 case "Lord Rodrik":
-                    image.src = "../img/sigils/stark.jpg";
+                    image.src = "/img/sigils/stark.jpg";
                     break;
                 case "Davos":
-                    image.src = "../img/sigils/seaworth.jpg";
+                    image.src = "/img/sigils/seaworth.jpg";
                     break;
                 case "Vicatarion Greyjoy":
-                    image.src = "../img/sigils/greyjoy.jpg";
+                    image.src = "/img/sigils/greyjoy.jpg";
                     break;
                 case "Cersei Lannister":
-                    image.src = "../img/sigils/lannister.jpg";
+                    image.src = "i/mg/sigils/lannister.jpg";
                     break;
                 case "Cersei and Tyrion":
-                    image.src = "../img/sigils/lannister.jpg";
+                    image.src = "/img/sigils/lannister.jpg";
                     break;
                 case "Jaime Lannister":
-                    image.src = "../img/sigils/lannister.jpg";
+                    image.src = "/img/sigils/lannister.jpg";
                     break;
                 case "Tyrion":
-                    image.src = "../img/sigils/lannister.jpg";
+                    image.src = "/img/sigils/lannister.jpg";
                     break;
                 case "Bronn":
-                    image.src = "../img/sigils/lannister.jpg";
+                    image.src = "/img/sigils/lannister.jpg";
                     break;
                 case "Samwell":
-                    image.src = "../img/sigils/tarly.jpg";
+                    image.src = "/img/sigils/tarly.jpg";
                     break;
                 case "The Hound":
-                    image.src = "../img/sigils/clegane.jpg";
+                    image.src = "/img/sigils/clegane.jpg";
                     break;
                 case "Littlefinger":
-                    image.src = "../img/sigils/baelish.jpg";
+                    image.src = "/img/sigils/baelish.jpg";
                     break;
                 case "Daenerys":
-                    image.src = "../img/sigils/targaryen.jpg";
+                    image.src = "/img/sigils/targaryen.jpg";
                     break;
                 case "Renly Baratheon":
-                    image.src = "../img/sigils/baratheon.jpg";
+                    image.src = "/img/sigils/baratheon.jpg";
                     break;
                 case "Brynden Tully":
-                    image.src = "../img/sigils/tully.jpg";
+                    image.src = "/img/sigils/tully.jpg";
                     break;
                 default:
-                    image.src = "../img/sigils/default.jpeg";
+                    image.src = "/img/sigils/default.jpeg";
                     break;
             }
         })
